525f5f31abbc1a9bbeed21df934b9f9804b8b5a1
Alla509 <agalamova042@gmail.com>
2026-04-10 15:55:51 +0500

    work with config in another class; determined names of windows in config

---
+++ robots/src/gui/ConfigManager.java
@@ -0,0 +1,96 @@
+package gui;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+import log.Logger;
+
+/**
+ * Управление сохранением и загрузкой конфигурации приложения.
+ * Файл хранится в домашнем каталоге пользователя.
+ */
+public class ConfigManager {
+    private static final String CONFIG_FILE = System.getProperty("user.home") + File.separator + ".robot-config.properties";
+    private final Properties props = new Properties();
+
+    public void load() {
+        File file = new File(CONFIG_FILE);
+        if (!file.exists()) return;
+        try (FileInputStream fis = new FileInputStream(file)) {
+            props.load(fis);
+        } catch (IOException e) {
+            Logger.debug("Не удалось загрузить конфигурацию: " + e.getMessage());
+        }
+    }
+
+    public void save() {
+        try (FileOutputStream fos = new FileOutputStream(CONFIG_FILE)) {
+            props.store(fos, "Robot program configuration");
+        } catch (IOException e) {
+            Logger.debug("Не удалось сохранить конфигурацию: " + e.getMessage());
+        }
+    }
+
+    public void setMainWindowBounds(int x, int y, int width, int height, int state) {
+        setProperty("main.x", x);
+        setProperty("main.y", y);
+        setProperty("main.width", width);
+        setProperty("main.height", height);
+        setProperty("main.state", state);
+    }
+
+    public int getMainWindowX(int defaultValue) { return getIntProperty("main.x", defaultValue); }
+    public int getMainWindowY(int defaultValue) { return getIntProperty("main.y", defaultValue); }
+    public int getMainWindowWidth(int defaultValue) { return getIntProperty("main.width", defaultValue); }
+    public int getMainWindowHeight(int defaultValue) { return getIntProperty("main.height", defaultValue); }
+    public int getMainWindowState(int defaultValue) { return getIntProperty("main.state", defaultValue); }
+
+    public void setWindowBounds(String windowName, int x, int y, int width, int height, boolean icon, boolean maximized) {
+        setProperty(key(windowName, "x"), x);
+        setProperty(key(windowName, "y"), y);
+        setProperty(key(windowName, "width"), width);
+        setProperty(key(windowName, "height"), height);
+        setProperty(key(windowName, "icon"), icon);
+        setProperty(key(windowName, "maximized"), maximized);
+    }
+
+    public boolean hasWindow(String windowName) {
+        return props.containsKey(key(windowName, "x"));
+    }
+
+    public int getWindowX(String windowName, int defaultValue) { return getIntProperty(key(windowName, "x"), defaultValue); }
+    public int getWindowY(String windowName, int defaultValue) { return getIntProperty(key(windowName, "y"), defaultValue); }
+    public int getWindowWidth(String windowName, int defaultValue) { return getIntProperty(key(windowName, "width"), defaultValue); }
+    public int getWindowHeight(String windowName, int defaultValue) { return getIntProperty(key(windowName, "height"), defaultValue); }
+    public boolean getWindowIcon(String windowName, boolean defaultValue) { return getBooleanProperty(key(windowName, "icon"), defaultValue); }
+    public boolean getWindowMaximized(String windowName, boolean defaultValue) { return getBooleanProperty(key(windowName, "maximized"), defaultValue); }
+
+    private String key(String windowName, String suffix) {
+        return windowName + "." + suffix;   // единственное место с конкатенацией (инкапсулировано)
+    }
+
+    private void setProperty(String key, int value) {
+        props.setProperty(key, Integer.toString(value));
+    }
+
+    private void setProperty(String key, boolean value) {
+        props.setProperty(key, Boolean.toString(value));
+    }
+
+    private int getIntProperty(String key, int defaultValue) {
+        String value = props.getProperty(key);
+        if (value == null) return defaultValue;
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
+    }
+
+    private boolean getBooleanProperty(String key, boolean defaultValue) {
+        String value = props.getProperty(key);
+        return value == null ? defaultValue : Boolean.parseBoolean(value);
+    }
+}
--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -2,57 +2,42 @@ package gui;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.WindowEvent;
 import java.awt.event.WindowAdapter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Properties;
-import javax.swing.JOptionPane;
+import java.awt.event.WindowEvent;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-
 import log.Logger;
 
-/**
- * Что требуется сделать:
- * 1. Метод создания меню перегружен функционалом и трудно читается. 
- * Следует разделить его на серию более простых методов (или вообще выделить отдельный класс).
- *
- */
-public class MainApplicationFrame extends JFrame
-{
+public class MainApplicationFrame extends JFrame {
+    private static final String LOG_WINDOW_TITLE = "Протокол работы";
+    private static final String GAME_WINDOW_TITLE = "Игровое поле";
+    private static final String LOG_WINDOW_NAME = "LogWindow";
+    private static final String GAME_WINDOW_NAME = "GameWindow";
+
     private final JDesktopPane desktopPane = new JDesktopPane();
-    private static final String CONFIG_FILE = System.getProperty("user.home") + File.separator + ".robot-config.properties";
+    private final ConfigManager configManager = new ConfigManager();
 
     public MainApplicationFrame() {
-        loadWindowState();
-        int inset = 50;
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        setBounds(inset, inset,
-                screenSize.width  - inset*2,
-                screenSize.height - inset*2);
+        configManager.load();
+        loadMainWindowState();
 
         setContentPane(desktopPane);
 
         LogWindow logWindow = createLogWindow();
         addWindow(logWindow);
-
         GameWindow gameWindow = new GameWindow();
-        gameWindow.setSize(400,  400);
+        gameWindow.setSize(400, 400);
         addWindow(gameWindow);
 
-        loadInternalWindowsState();
+        loadInternalWindowState(LOG_WINDOW_NAME, logWindow);
+        loadInternalWindowState(GAME_WINDOW_NAME, gameWindow);
 
-        MenuBarFactory menuFactory = new MenuBarFactory(this);
-        setJMenuBar(menuFactory.createMenuBar());
+        setJMenuBar(new MenuBarFactory(this).createMenuBar());
 
-        //закрытие приложения через крестик
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -62,25 +47,22 @@ public class MainApplicationFrame extends JFrame
     }
 
     public void exitApplication() {
-        //так как в UIManager уже установлен русский язык, то не нужно делать это дополнительно
-        saveWindowState();
-        int result = JOptionPane.showConfirmDialog(
-                this,
+        saveAllWindowsState();
+        configManager.save();
+
+        int result = JOptionPane.showConfirmDialog(this,
                 "Вы действительно хотите выйти из приложения?",
                 "Подтверждение выхода",
                 JOptionPane.YES_NO_OPTION,
-                JOptionPane.QUESTION_MESSAGE
-        );
-
+                JOptionPane.QUESTION_MESSAGE);
         if (result == JOptionPane.YES_OPTION) {
             System.exit(0);
         }
     }
 
-    protected LogWindow createLogWindow()
-    {
+    protected LogWindow createLogWindow() {
         LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
-        logWindow.setLocation(10,10);
+        logWindow.setLocation(10, 10);
         logWindow.setSize(300, 800);
         setMinimumSize(logWindow.getSize());
         logWindow.pack();
@@ -88,153 +70,73 @@ public class MainApplicationFrame extends JFrame
         return logWindow;
     }
 
-    protected void addWindow(JInternalFrame frame)
-    {
+    protected void addWindow(JInternalFrame frame) {
         desktopPane.add(frame);
         frame.setVisible(true);
     }
 
-    /**
-     * Метод для смены Look&Feel (публичный, чтобы был доступен из MenuBarFactory)
-     */
-    public void setLookAndFeel(String className)
-    {
-        try
-        {
+    public void setLookAndFeel(String className) {
+        try {
             UIManager.setLookAndFeel(className);
             RobotsProgram.setRussianUIManagerText();
             SwingUtilities.updateComponentTreeUI(this);
-        }
-        catch (ClassNotFoundException | InstantiationException
-               | IllegalAccessException | UnsupportedLookAndFeelException e)
-        {
-            // just ignore
+        } catch (Exception e) {
+            // ignore
         }
     }
 
-    /**
-     * Загружает состояние главного окна из файла конфигурации.
-     * Если файл отсутствует, устанавливает размеры по умолчанию.
-     */
-    private void loadWindowState() {
-        Properties props = new Properties();
-        File configFile = new File(CONFIG_FILE);
-        if (!configFile.exists()) {
-            // геометрия по умолчанию (как было в конструкторе)
-            int inset = 50;
-            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            setBounds(inset, inset,
-                    screenSize.width - inset * 2,
-                    screenSize.height - inset * 2);
-            return;
-        }
+    private void loadMainWindowState() {
+        int inset = 50;
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int defaultWidth = screenSize.width - inset * 2;
+        int defaultHeight = screenSize.height - inset * 2;
 
-        try (FileInputStream fis = new FileInputStream(configFile)) {
-            props.load(fis);
-
-            int x = Integer.parseInt(props.getProperty("main.x", "50"));
-            int y = Integer.parseInt(props.getProperty("main.y", "50"));
-            int width = Integer.parseInt(props.getProperty("main.width", "800"));
-            int height = Integer.parseInt(props.getProperty("main.height", "600"));
-            int state = Integer.parseInt(props.getProperty("main.state", String.valueOf(JFrame.NORMAL)));
-
-            setBounds(x, y, width, height);
-            setExtendedState(state);
-        } catch (IOException | NumberFormatException e) {
-            Logger.debug("Не удалось загрузить конфигурацию: " + e.getMessage());
-            // в случае ошибки используем геометрию по умолчанию
-            int inset = 50;
-            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            setBounds(inset, inset,
-                    screenSize.width - inset * 2,
-                    screenSize.height - inset * 2);
-        }
+        setBounds(configManager.getMainWindowX(inset),
+                configManager.getMainWindowY(inset),
+                configManager.getMainWindowWidth(defaultWidth),
+                configManager.getMainWindowHeight(defaultHeight));
+        setExtendedState(configManager.getMainWindowState(JFrame.NORMAL));
     }
 
-    /*
-     * Сохраняет состояние главного окна и всех внутренних окон в файл.
-        */
-    private void saveWindowState() {
-        Properties props = new Properties();
+    private void loadInternalWindowState(String windowName, JInternalFrame frame) {
+        if (!configManager.hasWindow(windowName)) return;
 
-        // Главное окно
-        props.setProperty("main.x", String.valueOf(getX()));
-        props.setProperty("main.y", String.valueOf(getY()));
-        props.setProperty("main.width", String.valueOf(getWidth()));
-        props.setProperty("main.height", String.valueOf(getHeight()));
-        props.setProperty("main.state", String.valueOf(getExtendedState()));
+        int x = configManager.getWindowX(windowName, frame.getX());
+        int y = configManager.getWindowY(windowName, frame.getY());
+        int w = configManager.getWindowWidth(windowName, frame.getWidth());
+        int h = configManager.getWindowHeight(windowName, frame.getHeight());
+        boolean icon = configManager.getWindowIcon(windowName, false);
+        boolean maximized = configManager.getWindowMaximized(windowName, false);
 
-        // Внутренние окна
-        JInternalFrame[] frames = desktopPane.getAllFrames();
-        for (int i = 0; i < frames.length; i++) {
-            JInternalFrame f = frames[i];
-            String title = f.getTitle();
-            if (title == null || title.isEmpty()) continue;
-
-            String prefix = "window." + i + ".";
-            props.setProperty(prefix + "title", title);
-            props.setProperty(prefix + "x", String.valueOf(f.getX()));
-            props.setProperty(prefix + "y", String.valueOf(f.getY()));
-            props.setProperty(prefix + "width", String.valueOf(f.getWidth()));
-            props.setProperty(prefix + "height", String.valueOf(f.getHeight()));
-            props.setProperty(prefix + "icon", String.valueOf(f.isIcon()));
-            props.setProperty(prefix + "maximized", String.valueOf(f.isMaximum()));
-        }
-
-        try (FileOutputStream fos = new FileOutputStream(CONFIG_FILE)) {
-            props.store(fos, "Robot program configuration");
-        } catch (IOException e) {
-            Logger.debug("Не удалось сохранить конфигурацию: " + e.getMessage());
-        }
-    }
-
-    /**
-     * Восстанавливает состояние внутренних окон после их создания.
-     */
-    private void loadInternalWindowsState() {
-        Properties props = new Properties();
-        File configFile = new File(CONFIG_FILE);
-        if (!configFile.exists()) return;
-
-        try (FileInputStream fis = new FileInputStream(configFile)) {
-            props.load(fis);
-
-            JInternalFrame[] frames = desktopPane.getAllFrames();
-            for (JInternalFrame frame : frames) {
-                String title = frame.getTitle();
-                if (title == null) continue;
-
-                // Ищем запись с таким же заголовком
-                for (String key : props.stringPropertyNames()) {
-                    if (key.endsWith(".title") && props.getProperty(key).equals(title)) {
-                        String prefix = key.substring(0, key.length() - 6); // убираем ".title"
-                        try {
-                            int x = Integer.parseInt(props.getProperty(prefix + ".x", "0"));
-                            int y = Integer.parseInt(props.getProperty(prefix + ".y", "0"));
-                            int w = Integer.parseInt(props.getProperty(prefix + ".width", "300"));
-                            int h = Integer.parseInt(props.getProperty(prefix + ".height", "200"));
-                            boolean icon = Boolean.parseBoolean(props.getProperty(prefix + ".icon", "false"));
-                            boolean maximized = Boolean.parseBoolean(props.getProperty(prefix + ".maximized", "false"));
-
-                            frame.setBounds(x, y, w, h);
-                            try {
-                                if (maximized) {
-                                    frame.setMaximum(true);
-                                } else if (icon) {
-                                    frame.setIcon(true);
-                                }
-                            } catch (java.beans.PropertyVetoException e) {
-                                Logger.debug("Не удалось установить состояние окна " + title + ": " + e.getMessage());
-                            }
-                        } catch (NumberFormatException e) {
-                            Logger.debug("Ошибка парсинга конфигурации для окна " + title);
-                        }
-                        break;
-                    }
-                }
+        frame.setBounds(x, y, w, h);
+        try {
+            if (maximized) {
+                frame.setMaximum(true);
+            } else if (icon) {
+                frame.setIcon(true);
             }
-        } catch (IOException e) {
-            Logger.debug("Не удалось загрузить конфигурацию внутренних окон: " + e.getMessage());
+        } catch (java.beans.PropertyVetoException e) {
+            Logger.debug("Не удалось восстановить состояние окна " + windowName + ": " + e.getMessage());
         }
+    }
+
+    private void saveAllWindowsState() {
+        configManager.setMainWindowBounds(getX(), getY(), getWidth(), getHeight(), getExtendedState());
+
+        for (JInternalFrame frame : desktopPane.getAllFrames()) {
+            String windowName = getWindowNameByTitle(frame.getTitle());
+            if (windowName != null) {
+                configManager.setWindowBounds(windowName,
+                        frame.getX(), frame.getY(),
+                        frame.getWidth(), frame.getHeight(),
+                        frame.isIcon(), frame.isMaximum());
+            }
+        }
+    }
+
+    private String getWindowNameByTitle(String title) {
+        if (LOG_WINDOW_TITLE.equals(title)) return LOG_WINDOW_NAME;
+        if (GAME_WINDOW_TITLE.equals(title)) return GAME_WINDOW_NAME;
+        return null;
     }
 }
